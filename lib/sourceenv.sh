a6ac6ebf5e633a5430950fe64de3fd4e7ff598dc
pueri <pyro@outlook.at>
2020-01-03 08:45:55 +0100

    Use for loop for searching for custom env file

--- lib/sourceenv.sh
+++ lib/sourceenv.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [[ -f $(dirname ${BASH_SOURCE[0]:-$0})/../.bashutils.env ]]; then
-  source $(dirname ${BASH_SOURCE[0]:-$0})/../.bashutils.env
-elif [[ -f $(dirname ${BASH_SOURCE[0]:-$0})/../../.bashutils.env ]]; then
-  source $(dirname ${BASH_SOURCE[0]:-$0})/../../.bashutils.env
-fi
+bash_utils_custom_env_paths=(
+  "../.bashutils.env"
+  "../.env"
+  "../../.bashutils.env"
+)
+
+for path in ${bash_utils_custom_env_paths[@]}; do
+  [[ ! -f $(dirname ${BASH_SOURCE[0]:-$0})/$path ]] && continue
+
+  source $(dirname ${BASH_SOURCE[0]:-$0})/$path
+  break;
+done
