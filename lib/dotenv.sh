b24c6dd34ed57420d86f7a96f73aa9dc9b862613
pueri <pyro@outlook.at>
2020-01-18 10:03:27 +0100

    Add source_dotenv function to dotenv.sh

--- lib/dotenv.sh
+++ lib/dotenv.sh
@@ -33,9 +33,23 @@ function export_to_env() {
   grep -E "^export\s\S+=" $1 | sed 's/export\s//'
 }
 
+function source_dotenv() {
+  if [[ ! -f $1 ]]; then
+    echo "source_dotenv: File '$1' does not exist" >&2
+    return 1
+  fi
+
+  if ! dotenv_is_valid $1; then
+    echo "source_dotenv: File '$1' is not a valid dotenv file" >&2
+    return 1
+  fi
+
+  source $1
+}
+
 function source_dotenvs() {
   for i in $@; do
     [[ ! -f $i ]] && continue
-    source $i
+    source_dotenv $i
   done
 }
