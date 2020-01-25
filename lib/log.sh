f02dcae20e8af8bcaf36e1d820bdee3f3f40a2b6
pueri <pyro@outlook.at>
2020-01-25 13:26:55 +0100

    Refactor log utility.sh using chalk

--- lib/log.sh
+++ lib/log.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-source $(dirname ${BASH_SOURCE[0]:-$0})/echo.sh
+source $(dirname ${BASH_SOURCE[0]:-$0})/chalk.sh
 source $(dirname ${BASH_SOURCE[0]:-$0})/log.env
 source $(dirname ${BASH_SOURCE[0]:-$0})/sourceenv.sh
 
-function handle_log_file() {
-  if [[ -z $BASH_UTILS_LOG_PATH ]]; then
-    return
-  fi
+function writelog() {
+  local files
+  local level=info
+  local OPTIND
+  local prefix=""
+  
+  while getopts 'f:l:s' flag; do
+    case $flag in
+      f) filed=$OPTARG ;;
+      l)
+        case $OPTARG in
+          emph) level=emph ;;
+          error) level=error ;;
+          success) level=success ;;
+          warn | warning) level=warning ;;
+          *) level=info ;;
+        esac
+        ;;
+    esac
+  done
+
+  [[ -z $BASH_UTILS_LOG_PATH ]] && return
+
+  shift $(($OPTIND - 1))
+
+  [[ ! -z $BASH_UTILS_LOG_PREFIX ]] && prefix="$BASH_UTILS_LOG_PREFIX "
+  prefix="$prefix$(printf '%-8s' $level): "
+  pretix="${BASH_UTILS_PREFIX_COLOR}${prefix}${BASH_UTILS_DEFAULT_COLOR}"
 
   if [[ -f $BASH_UTILS_LOG_PATH ]]; then
     local size=$(stat -c %s $BASH_UTILS_LOG_PATH)
@@ -23,36 +47,15 @@ function handle_log_file() {
     mkdir -p $(dirname $BASH_UTILS_LOG_PATH)
     touch $BASH_UTILS_LOG_PATH
   fi
-}
-
-function log_emph() {
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-  handle_log_file
-  echo -e "$(echo_prefix "emph    :") $(echo_emph $@)" >> $BASH_UTILS_LOG_PATH
-}
-
-function log_error() {
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-  handle_log_file
-  echo -e "$(echo_prefix "error   :") $(echo_error $@)" >> $BASH_UTILS_LOG_PATH
-}
-
-function log_info() {
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-  handle_log_file
-  echo -e "$(echo_prefix "info    :") $(echo_info $@)" >> $BASH_UTILS_LOG_PATH
-}
-
-function log_success() {
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-  handle_log_file
-  echo -e "$(echo_prefix "success :") $(echo_success $@)" >> $BASH_UTILS_LOG_PATH
-}
-
-function log_warn() {
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-  handle_log_file
-  echo -e "$(echo_prefix "warning :") $(echo_warn $@)" >> $BASH_UTILS_LOG_PATH
+  
+  if [[ $# -gt 0 ]]; then
+    echo -e "$prefix$(echo $@ | chalk -l $level)" >> $BASH_UTILS_LOG_PATH
+    return
+  fi
+  
+  while read data; do
+    echo -e "$prefix$(echo $data | chalk -l $level)" >> $BASH_UTILS_LOG_PATH
+  done
 }
 
 function log() {
@@ -72,54 +75,13 @@ function log() {
   shift $(($OPTIND - 1))
 
   if [[ $# -gt 0 ]]; then
-    case $level in
-      emph)
-        [[ $silent != "true" ]] && echo_emph "$@"
-        [[ $echoonly != "true" ]] && log_emph "$@"
-        ;;
-      error)
-        [[ $silent != "true" ]] && echo_error "$@"
-        [[ $echoonly != "true" ]] && log_error "$@"
-        ;;
-      success)
-        [[ $silent != "true" ]] && echo_success "$@"
-        [[ $echoonly != "true" ]] && log_success "$@"
-        ;;
-      warn | warning)
-        [[ $silent != "true" ]] && echo_warn "$@"
-        [[ $echoonly != "true" ]] && log_warn "$@"
-        ;;
-      *)
-        [[ $silent != "true" ]] && echo_info "$@"
-        [[ $echoonly != "true" ]] && log_info "$@"
-        ;;
-    esac
-
+    [[ $silent != "true" ]] && echo $@ | chalk -l $level
+    [[ $echoonly != "true" ]] && echo $@ | writelog -l $level
     return
   fi
 
   while read data; do
-    case $level in
-      emph)
-        [[ $silent != "true" ]] && echo_emph "$data"
-        [[ $echoonly != "true" ]] && log_emph "$data"
-        ;;
-      error)
-        [[ $silent != "true" ]] && echo_error "$data"
-        [[ $echoonly != "true" ]] && log_error "$data"
-        ;;
-      success)
-        [[ $silent != "true" ]] && echo_success "$data"
-        [[ $echoonly != "true" ]] && log_success "$data"
-        ;;
-      warn | warning)
-        [[ $silent != "true" ]] && echo_warn "$data"
-        [[ $echoonly != "true" ]] && log_warn "$data"
-        ;;
-      *)
-        [[ $silent != "true" ]] && echo_info "$data"
-        [[ $echoonly != "true" ]] && log_info "$data"
-        ;;
-    esac
+    [[ $silent != "true" ]] && echo $data | chalk -l $level
+    [[ $echoonly != "true" ]] && echo $data | writelog -l $level
   done
 }
