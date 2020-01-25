c99099600b04e048ae8c916726c3b299a262f57a
pueri <pyro@outlook.at>
2020-01-25 12:05:02 +0100

    Add log function to log.sh

--- lib/log.sh
+++ lib/log.sh
@@ -26,36 +26,100 @@ function handle_log_file() {
 }
 
 function log_emph() {
-  echo_emph $@
   [[ -z $BASH_UTILS_LOG_PATH ]] && return
   handle_log_file
   echo -e "$(echo_prefix "emph    :") $(echo_emph $@)" >> $BASH_UTILS_LOG_PATH
 }
 
 function log_error() {
-  echo_error $@
   [[ -z $BASH_UTILS_LOG_PATH ]] && return
   handle_log_file
   echo -e "$(echo_prefix "error   :") $(echo_error $@)" >> $BASH_UTILS_LOG_PATH
 }
 
 function log_info() {
-  echo -e $@
   [[ -z $BASH_UTILS_LOG_PATH ]] && return
   handle_log_file
   echo -e "$(echo_prefix "info    :") $(echo_info $@)" >> $BASH_UTILS_LOG_PATH
 }
 
 function log_success() {
-  echo_success $@
   [[ -z $BASH_UTILS_LOG_PATH ]] && return
   handle_log_file
   echo -e "$(echo_prefix "success :") $(echo_success $@)" >> $BASH_UTILS_LOG_PATH
 }
 
 function log_warn() {
-  echo_warn $@
   [[ -z $BASH_UTILS_LOG_PATH ]] && return
   handle_log_file
   echo -e "$(echo_prefix "warning :") $(echo_warn $@)" >> $BASH_UTILS_LOG_PATH
+}
+
+function log() {
+  local echoonly=false
+  local level=info
+  local OPTIND
+  local silent=false
+
+  while getopts 'el:s' flag; do
+    case $flag in
+      e) echoonly=true ;;
+      l) level=$OPTARG ;;
+      s) silent=true ;;
+    esac
+  done
+
+  shift $(($OPTIND - 1))
+
+  if [[ $# -gt 0 ]]; then
+    case $level in
+      emph)
+        [[ $silent != "true" ]] && echo_emph "$@"
+        [[ $echoonly != "true" ]] && log_emph "$@"
+        ;;
+      error)
+        [[ $silent != "true" ]] && echo_error "$@"
+        [[ $echoonly != "true" ]] && log_error "$@"
+        ;;
+      success)
+        [[ $silent != "true" ]] && echo_success "$@"
+        [[ $echoonly != "true" ]] && log_success "$@"
+        ;;
+      warn | warning)
+        [[ $silent != "true" ]] && echo_warn "$@"
+        [[ $echoonly != "true" ]] && log_warn "$@"
+        ;;
+      *)
+        [[ $silent != "true" ]] && echo_info "$@"
+        [[ $echoonly != "true" ]] && log_info "$@"
+        ;;
+    esac
+
+    return
+  fi
+
+  while read data; do
+    case $level in
+      emph)
+        [[ $silent != "true" ]] && echo_emph "$data"
+        [[ $echoonly != "true" ]] && log_emph "$data"
+        ;;
+      error)
+        [[ $silent != "true" ]] && echo_error "$data"
+        [[ $echoonly != "true" ]] && log_error "$data"
+        ;;
+      success)
+        [[ $silent != "true" ]] && echo_success "$data"
+        [[ $echoonly != "true" ]] && log_success "$data"
+        ;;
+      warn | warning)
+        [[ $silent != "true" ]] && echo_warn "$data"
+        [[ $echoonly != "true" ]] && log_warn "$data"
+        ;;
+      *)
+        [[ $silent != "true" ]] && echo_info "$data"
+        [[ $echoonly != "true" ]] && log_info "$data"
+        ;;
+    esac
+  done
 }
