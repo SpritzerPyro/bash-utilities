432eca407662e31940287534f37396cdd16b96c9
pueri <pyro@outlook.at>
2020-03-27 06:36:21 +0100

    Remove shebang from sourceenv.sh

--- lib/sourceenv.sh
+++ lib/sourceenv.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-for var in $(cat $(dirname ${BASH_SOURCE[0]:-$0})/../config/variables); do
+for var in $(cat $(dirname ${BASH_SOURCE[0]})/../config/variables); do
   [[ -n ${!var+set} ]] && continue
 
   for path in \
-    $(dirname ${BASH_SOURCE[0]:-$0})/../config/default.env \
-    $(dirname ${BASH_SOURCE[0]:-$0})/../../.bashutils.env \
-    $(dirname ${BASH_SOURCE[0]:-$0})/../../.env \
-    $(dirname ${BASH_SOURCE[0]:-$0})/../.bashutils.env \
-    $(dirname ${BASH_SOURCE[0]:-$0})/../.env \
+    $(dirname ${BASH_SOURCE[0]})/../config/default.env \
+    $(dirname ${BASH_SOURCE[0]})/../../.bashutils.env \
+    $(dirname ${BASH_SOURCE[0]})/../../.env \
+    $(dirname ${BASH_SOURCE[0]})/../.bashutils.env \
+    $(dirname ${BASH_SOURCE[0]})/../.env \
   ; do
     [[ ! -f "$path" ]] && continue
 
