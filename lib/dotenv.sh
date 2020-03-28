6a41ae4a6ef0ad4bd48c2bdcac2a6b85278014c4
pueri <pyro@outlook.at>
2020-03-28 12:07:55 +0100

    Add -v flag to dotenv to specify variable regex

--- lib/dotenv.sh
+++ lib/dotenv.sh
@@ -1,35 +1,39 @@
 function dotenv::grep() {
-  local OPTIND flag
-  local regex="^\s*(export\s+)?[a-zA-Z_]+[a-zA-Z0-9_]*=[a-zA-Z0-9_]*"
+  local OPTARG OPTIND flag
   local flags=(--directories='skip' --extended-regexp --no-filename)
+  local var_regex="[a-zA-Z_]+[a-zA-Z0-9_]*"
 
-  while getopts 's' flag; do
+  while getopts 'isv:' flag; do
     case "${flag}" in
+      i) flags+=(--ignore-case) ;;
       s) flags+=(--no-messages) ;;
+      v) var_regex="${OPTARG}" ;;
     esac
   done
 
   shift $(($OPTIND - 1))
 
-  if [[ -z "$@" ]] && (( "${#flags[@]}" < 4 )); then
+  if [[ -z "$@" ]] && [[ "${flags[@]}" =~ "--no-messages" ]]; then
     echo "dotenv::grep: No files specified" >&2
     return 1
   fi
 
+  local regex="^\s*(export\s+)?${var_regex}="
   local res=$(grep "${flags[@]}" "${regex}" ${@:-""} || echo "")
 
   echo "${res}" | sed -E 's/^\s*(export\s+)?//'
 }
 
 function dotenv::source() {
-  local OPTIND flag
+  local OPTARG OPTIND flag
   local allexport
   local grep_flags=()
 
-  while getopts 'as' flag; do
+  while getopts 'aisv:' flag; do
     case "${flag}" in
       a) allexport="true" ;;
-      s) grep_flags+=(-s) ;;
+      i|s) grep_flags+=("-${flag}") ;;
+      v) grep_flags+=("-v${OPTARG}") ;;
     esac
   done
 
@@ -37,7 +41,7 @@ function dotenv::source() {
 
   if [[ "${allexport}" == "true" ]]; then set -a; fi
 
-  source <(dotenv::grep "${grep_flags[@]}" "${@}")
+  source <(dotenv::grep "${grep_flags[@]}" "$@")
 
   if [[ "${allexport}" == "true" ]]; then set +a; fi
 }
