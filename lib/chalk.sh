aff6cf40cde1fbae1f8e4c24e8cac16b94828b07
pueri <pyro@outlook.at>
2020-03-28 09:45:54 +0100

    Refactor chalk function in chalk.sh

--- lib/chalk.sh
+++ lib/chalk.sh
@@ -1,36 +1,31 @@
 function chalk() {
+  local color="${BASH_UTILS_COLOR_INFO}"
+  local default_color="${BASH_UTILS_COLOR_DEFAULT}"
   local flags=(-e)
-  local level=info
   local OPTARG OPTIND
-  
+
   while getopts 'l:n' flag; do
     case "${flag}" in
-      l) level="${OPTARG}" ;;
-      n) flags+=(-n) ;;
+      l) 
+        case "${OPTARG}" in
+          emph) color="${BASH_UTILS_COLOR_EMPH}" ;;
+          error) color="${BASH_UTILS_COLOR_ERROR}" ;;
+          success) color="${BASH_UTILS_COLOR_SUCCESS}" ;;
+          warn | warning) color="${BASH_UTILS_COLOR_WARN}" ;;
+        esac
+        ;;
+      n) flags+=("-${flag}") ;;
     esac
   done
-  
+
   shift $(($OPTIND - 1))
 
-  if [[ $# -gt 0 ]]; then
-    case $level in
-      emph) echo "${flags[@]}" "${BASH_UTILS_COLOR_EMPH}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      error) echo "${flags[@]}" "${BASH_UTILS_COLOR_ERROR}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      success) echo "${flags[@]}" "${BASH_UTILS_COLOR_SUCCESS}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      warn | warning) echo "${flags[@]}" "${BASH_UTILS_COLOR_WARN}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      *) echo "${flags[@]}" "${BASH_UTILS_COLOR_INFO}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-    esac
-    
+  if (( "$#" > 0 )); then
+    echo "${flags[@]}" "${color}$@${default_color}"
     return
   fi
-  
+
   while read data; do
-    case $level in
-      emph) echo "${flags[@]}" "${BASH_UTILS_COLOR_EMPH}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      error) echo "${flags[@]}" "${BASH_UTILS_COLOR_ERROR}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      success) echo "${flags[@]}" "${BASH_UTILS_COLOR_SUCCESS}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      warn | warning) echo "${flags[@]}" "${BASH_UTILS_COLOR_WARN}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      *) echo "${flags[@]}" "${BASH_UTILS_COLOR_INFO}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-    esac
+    echo "${flags[@]}" "${color}${data}${default_color}"
   done
 }
