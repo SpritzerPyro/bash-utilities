98ac84b47ba33b67ae980ad0537376695007c067
pueri <pyro@outlook.at>
2020-03-28 08:32:50 +0100

    Add echo's -n flag to chalk function

--- lib/chalk.sh
+++ lib/chalk.sh
@@ -1,22 +1,24 @@
 function chalk() {
+  local flags=(-e)
   local level=info
-  local OPTIND
+  local OPTARG OPTIND
   
-  while getopts 'l:' flag; do
-    case $flag in
-      l) level=$OPTARG ;;
+  while getopts 'l:n' flag; do
+    case "${flag}" in
+      l) level="${OPTARG}" ;;
+      n) flags+=(-n) ;;
     esac
   done
   
   shift $(($OPTIND - 1))
-  
+
   if [[ $# -gt 0 ]]; then
     case $level in
-      emph) echo -e "${BASH_UTILS_COLOR_EMPH}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      error) echo -e "${BASH_UTILS_COLOR_ERROR}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      success) echo -e "${BASH_UTILS_COLOR_SUCCESS}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      warn | warning) echo -e "${BASH_UTILS_COLOR_WARN}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
-      *) echo -e "${BASH_UTILS_COLOR_INFO}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
+      emph) echo "${flags[@]}" "${BASH_UTILS_COLOR_EMPH}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
+      error) echo "${flags[@]}" "${BASH_UTILS_COLOR_ERROR}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
+      success) echo "${flags[@]}" "${BASH_UTILS_COLOR_SUCCESS}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
+      warn | warning) echo "${flags[@]}" "${BASH_UTILS_COLOR_WARN}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
+      *) echo "${flags[@]}" "${BASH_UTILS_COLOR_INFO}$@${BASH_UTILS_COLOR_DEFAULT}" ;;
     esac
     
     return
@@ -24,11 +26,11 @@ function chalk() {
   
   while read data; do
     case $level in
-      emph) echo -e "${BASH_UTILS_COLOR_EMPH}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      error) echo -e "${BASH_UTILS_COLOR_ERROR}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      success) echo -e "${BASH_UTILS_COLOR_SUCCESS}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      warn | warning) echo -e "${BASH_UTILS_COLOR_WARN}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
-      *) echo -e "${BASH_UTILS_COLOR_INFO}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
+      emph) echo "${flags[@]}" "${BASH_UTILS_COLOR_EMPH}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
+      error) echo "${flags[@]}" "${BASH_UTILS_COLOR_ERROR}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
+      success) echo "${flags[@]}" "${BASH_UTILS_COLOR_SUCCESS}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
+      warn | warning) echo "${flags[@]}" "${BASH_UTILS_COLOR_WARN}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
+      *) echo "${flags[@]}" "${BASH_UTILS_COLOR_INFO}$data${BASH_UTILS_COLOR_DEFAULT}" ;;
     esac
   done
 }
