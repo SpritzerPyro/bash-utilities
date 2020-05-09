789aed1a50a5701950f969d16e707a3bd14ee380
pueri <pyro@outlook.at>
2020-05-09 14:20:35 +0200

    Define DIRS variable in a config::init function

--- lib/config.sh
+++ lib/config.sh
@@ -2,9 +2,31 @@ function config::exit_trap() {
   [[ $1 == "0" ]] && return 0
 
   IFS=' ' read -ra caller_array <<< "$(caller)"
-  caller_path=$(readlink -f ${caller_array[1]})
+  caller_path=$(readlink -f "${caller_array[1]}")
 
-  echo "$caller_path exited with code $1" | log -l error
+  echo "${caller_path} exited with code $1" | log -l error
+}
+
+function config::init() {
+  export USER="${USER:-"${LOGNAME}"}"
+
+  declare -A -g DIRS
+  local -r home_dir="${HOME:-"/home/${USER}"}"
+  local -r lib_dir=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+  local -r media_dir="/media/${USER}"
+
+  DIRS[butils_lib]="${lib_dir}"
+  DIRS[butils]=$(readlink -f "${lib_dir}/..")
+  DIRS[script]=$(dirname $(readlink -f "$0"))
+  DIRS[working]=$(pwd)
+
+  if [[ -d "${home_dir}" ]]; then
+    DIRS[home]="${home_dir}"
+  fi
+
+  if [[ -d "${media_dir}" ]]; then
+    DIRS[media]="${media_dir}"
+  fi
 }
 
 function config::source() {
