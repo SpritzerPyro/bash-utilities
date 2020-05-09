d1bfc701002d38350b8dece778aeed65c7370583
pueri <pyro@outlook.at>
2020-05-09 16:21:31 +0200

    Refactor log::write function

--- lib/log.sh
+++ lib/log.sh
@@ -1,64 +1,63 @@
 source $(dirname ${BASH_SOURCE[0]})/chalk.sh
 
 function log::write() {
-  local level=info
-  local severity=info
-  local OPTIND
+  [[ -z "${BASH_UTILS_LOG_PATH}" ]] && return
+
+  local -A level=([color]=info [severity]=info)
+  local flag OPTARG OPTIND
   local prefix=""
 
-  while getopts 'l:s' flag; do
-    case $flag in
+  while getopts 'l:' flag; do
+    case "${flag}" in
       l)
-        case $OPTARG in
-          emph) level=emph ;;
-          error)
-            level=error
-            severity=error
-            ;;
-          success) level=success ;;
-          warn | warning)
-            level=warning
-            severity=warning
-            ;;
-          *) level=info ;;
+        case "${OPTARG}" in
+          emph) level[color]=emph ;;
+          error) level=([color]=error [severity]=error) ;;
+          success) level[color]=success ;;
+          warn | warning) level=([color]=warn [severity]=warn) ;;
         esac
         ;;
     esac
   done
 
-  [[ -z $BASH_UTILS_LOG_PATH ]] && return
-
-  shift $(($OPTIND - 1))
+  shift $(( ${OPTIND} - 1 ))
 
   if [[ ! -z "${BASH_UTILS_LOG_TIME_FORMAT}" ]]; then
     prefix="[$(date +"${BASH_UTILS_LOG_TIME_FORMAT}")] "
   fi
 
-  prefix="$prefix$(printf '%-8s' "${severity}"): "
+  prefix="${prefix}$(printf '%-6s' "${level[severity]}"): "
   prefix="${BASH_UTILS_COLOR_PREFIX}${prefix}${BASH_UTILS_COLOR_DEFAULT}"
 
-  if [[ -f $BASH_UTILS_LOG_PATH ]]; then
+  if [[ -f "${BASH_UTILS_LOG_PATH}" ]]; then
     local size=$(stat -c %s $BASH_UTILS_LOG_PATH)
 
-    if [[ $size -gt $BASH_UTILS_LOG_MAX_SIZE ]]; then
+    if (( ${size} > ${BASH_UTILS_LOG_MAX_SIZE} )); then
       local i=1
-      while [[ -f $BASH_UTILS_LOG_PATH.$i ]]; do ((i=i+1)); done
-      mv $BASH_UTILS_LOG_PATH $BASH_UTILS_LOG_PATH.$i
+
+      while [[ -f "${BASH_UTILS_LOG_PATH}.${i}" ]]; do
+        (( i = i + 1 ));
+      done
+
+      mv "${BASH_UTILS_LOG_PATH}" "${BASH_UTILS_LOG_PATH}.${i}"
     fi
   fi
 
-  if [[ ! -f $BASH_UTILS_LOG_PATH ]]; then
-    mkdir -p $(dirname $BASH_UTILS_LOG_PATH)
-    touch $BASH_UTILS_LOG_PATH
+  if [[ ! -f "${BASH_UTILS_LOG_PATH}" ]]; then
+    mkdir -p $(dirname "${BASH_UTILS_LOG_PATH}")
+    touch "${BASH_UTILS_LOG_PATH}"
   fi
 
-  if [[ $# -gt 0 ]]; then
-    echo -e "$prefix$(echo $@ | chalk -l $level)" | tee -a $BASH_UTILS_LOG_PATH >/dev/null
+  if (( $# > 0 )); then
+    echo -e "${prefix}$(chalk -l "${level[color]}" "$@")" \
+      | tee -a "${BASH_UTILS_LOG_PATH}" >/dev/null
+
     return
   fi
 
   while read data; do
-    echo -e "$prefix$(echo $data | chalk -l $level)" | tee -a $BASH_UTILS_LOG_PATH >/dev/null
+    echo -e "${prefix}$(chalk -l "${level[color]}" "${data}")" \
+      | tee -a $BASH_UTILS_LOG_PATH >/dev/null
   done
 }
 
