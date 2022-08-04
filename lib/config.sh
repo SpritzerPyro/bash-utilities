898f38a151e3c8a63410926dc650d1f85efa7a7d
SpritzerPyro <pyro@outlook.at>
2022-08-04 11:44:41 +0200

    Use no-op expansion to set default variables

--- lib/config.sh
+++ lib/config.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=bash
+
 function _config::arg_state() {
   local -r option=$(set -o | grep "$1" || echo "")
 
@@ -11,7 +13,7 @@ function _config::init_dirs() {
   local -r home_dir="${HOME:-"/home/${USER}"}"
   local -r media_dir="/media/${USER}"
 
-  DIRS[script]=$(dirname $(readlink -f "$0"))
+  DIRS[script]=$(dirname "$(readlink -f "$0")")
   DIRS[working]=$(pwd)
 
   if [[ -d "${home_dir}" ]]; then
@@ -19,6 +21,7 @@ function _config::init_dirs() {
   fi
 
   if [[ -d "${media_dir}" ]]; then
+    # shellcheck disable=SC2034
     DIRS[media]="${media_dir}"
   fi
 }
@@ -35,7 +38,7 @@ function _config::init_log() {
     [warn]=WARN
   )
 
-  for i in ${!BUTILS_LOG_LEVELS[@]} default prefix; do
+  for i in "${!BUTILS_LOG_LEVELS[@]}" default prefix; do
     local varname="BUTILS_COLOR_${i^^}"
 
     BUTILS_COLORS["${i}"]="${!varname:-"${BUTILS_COLOR_DEFAULT}"}"
@@ -46,7 +49,7 @@ function _config::log_info() {
   local _level=info
   local i
 
-  for i in ${!BUTILS_LOG_LEVELS[@]}; do
+  for i in "${!BUTILS_LOG_LEVELS[@]}"; do
     [[ "$2" != "${i}" ]] && continue
 
     _level="${i}"
@@ -58,19 +61,22 @@ function _config::log_info() {
   eval "$1=([color]=\"${_color}\" [level]=\"${_level}\" [key]=\"${_key}\")"
 }
 
-function _config::source {
-  local -r cfg_dir="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))/../config"
-  local variable
-
-  for variable in $(cat "${cfg_dir}/variables"); do
-    [[ -n "${!variable+set}" ]] && continue
-
-    source <(grep -s "^${variable}=" "${cfg_dir}/default.env")
-  done
+function _config::load_env {
+  : "${BUTILS_COLOR_DEBUG:="\\033[0;36m"}"
+  : "${BUTILS_COLOR_DEFAULT:="\\033[0m"}"
+  : "${BUTILS_COLOR_EMPH:="\\033[0;34m"}"
+  : "${BUTILS_COLOR_ERROR:="\\033[0;31m"}"
+  : "${BUTILS_COLOR_INFO:="\\033[0m"}"
+  : "${BUTILS_COLOR_PREFIX:="\\033[0;90m"}"
+  : "${BUTILS_COLOR_SUCCESS:="\\033[0;32m"}"
+  : "${BUTILS_COLOR_WARN:="\\033[0;33m"}"
+  : "${BUTILS_LOG_MAX_SIZE:=20971520}"
+  : "${BUTILS_LOG_MULTILINE_PREFIX="> "}"
+  : "${BUTILS_LOG_TIME_FORMAT:="%Y-%m-%dT%H:%M:%S.%3N%:z"}"
 }
 
 function _config::init {
-  _config::source
+  _config::load_env
   _config::init_dirs
   _config::init_log
 }
