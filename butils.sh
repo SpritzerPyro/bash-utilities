4787ae8f81b3658844dd07f0b4e4b8b1bc8a870d
SpritzerPyro <pyro@outlook.at>
2024-01-23 11:56:54 +0100

    Remove the docker.sh library

--- butils.sh
+++ butils.sh
@@ -6,42 +6,43 @@ function butils::init() {
   while getopts 'au:' flag; do
     case "${flag}" in
       a)
-        for i in chalk checks docker dotenv log query; do
+        for i in chalk checks dotenv log query; do
           butils::use "${i}"
         done
         ;;
       u) butils::use "${OPTARG}" ;;
+      *) echo "Invalid flag: ${flag}" >&2 ;;
     esac
   done
 }
 
 function butils::use() {
-  local -r lib_dir="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))/lib"
+  local -r lib_dir="$(
+    cd -- "$(dirname "${BASH_SOURCE[0]}")/lib" || exit
+    pwd
+  )"
 
   case "$1" in
-    chalk|chalks)
+    chalk | chalks)
       source "${lib_dir}/chalk.sh"
       chalk::init
       ;;
-    check|checks)
+    check | checks)
       source "${lib_dir}/checks.sh"
       ;;
-    config|configs)
+    config | configs)
       source "${lib_dir}/config.sh"
       _config::init
-      ;;
-    docker|docker-compose|compose)
-      source "${lib_dir}/docker.sh"
       ;;
     dotenv)
       source "${lib_dir}/dotenv.sh"
       ;;
-    log|logs|logging)
+    log | logs | logging)
       source "${lib_dir}/chalk.sh"
       source "${lib_dir}/log.sh"
       log::init
       ;;
-    query|queries)
+    query | queries)
       source "${lib_dir}/checks.sh"
       source "${lib_dir}/query.sh"
       ;;
@@ -51,6 +52,7 @@ function butils::use() {
         return 1
       fi
 
+      # shellcheck disable=SC1090
       source "${lib_dir}/$1.sh"
       ;;
   esac
